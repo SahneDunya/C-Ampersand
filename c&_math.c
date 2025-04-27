d5be851e02178e6fb44fbc0ac208fb211b19e126
Sahne Dünya <ilgazgozen@gmail.com>
2025-04-27 18:02:17 +0300

    c&_math.c

--- c&_math.c
+++ c&_math.c
@@ -1,49 +1,55 @@
 #include "c&_math.h"
-#include <math.h>   // Standart C matematik fonksiyonları
-#include <stdlib.h> // abs fonksiyonu için
+
+// Standart C matematik fonksiyonları.
+// ÖNEMLİ: Bu implementasyon, SAHNE64'ün C araç zincirinin
+// <math.h> ve <stdlib.h>'de bildirilen standart C matematik
+// fonksiyonlarının no_std ortamında kullanılabilen bir
+// implementasyonunu (libm) sağladığı varsayımına dayanmaktadır.
+// Aksi takdirde, bu çağrılar derlenemez veya çalışma zamanında hata verir.
+#include <math.h>   // sqrt, pow, sin, cos, tan, floor, ceil, round, log, log10 için
+#include <stdlib.h> // abs için (C99 ve sonrası math.h de içerebilir)
+
 
 int c_ampersand_abs(int n) {
-    return abs(n);
+    return abs(n);
 }
 
 double c_ampersand_sqrt(double x) {
-    return sqrt(x);
+    return sqrt(x);
 }
 
 double c_ampersand_pow(double x, double y) {
-    return pow(x, y);
+    return pow(x, y);
 }
 
 double c_ampersand_sin(double x) {
-    return sin(x);
+    return sin(x);
 }
 
 double c_ampersand_cos(double x) {
-    return cos(x);
+    return cos(x);
 }
 
 double c_ampersand_tan(double x) {
-    return tan(x);
+    return tan(x);
 }
 
 double c_ampersand_floor(double x) {
-    return floor(x);
+    return floor(x);
 }
 
 double c_ampersand_ceil(double x) {
-    return ceil(x);
+    return ceil(x);
 }
 
 double c_ampersand_round(double x) {
-    return round(x);
+    return round(x);
 }
 
 double c_ampersand_log(double x) {
-    return log(x);
+    return log(x);
 }
 
 double c_ampersand_log10(double x) {
-    return log10(x);
+    return log10(x);
 }
-
-// ... diğer matematiksel fonksiyonların implementasyonları buraya gelebilir
