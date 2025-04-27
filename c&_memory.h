d8c92f9b631b879daabc2666dbdc9350beecf20e
Sahne Dünya <ilgazgozen@gmail.com>
2025-04-27 18:05:23 +0300

    c&_memory.h

--- c&_memory.h
+++ c&_memory.h
@@ -1,14 +1,28 @@
 #ifndef C_AMPERSAND_MEMORY_H
 #define C_AMPERSAND_MEMORY_H
 
-#include "c&_stdlib.h" // Temel tipler ve hata yönetimi için
-#include <stddef.h> // size_t için
+// Temel C& tipler ve hata yönetimi için (c_ampersand_result, C_AMPERSAND_* hataları)
+#include "c&_stdlib.h"
+// Sahne64 API başlık dosyası (Bellek yönetimi fonksiyonları ve hata tipleri için)
+#include "sahne.h"
+
+#include <stddef.h> // size_t, NULL için
 
 // Belirli boyutta bellek ayırır.
+// Bellek Sahne64 API'si kullanılarak tahsis edilir.
+// size: Tahsis edilecek bellek boyutu (byte).
+// ptr: Tahsis edilen belleğin adresinin yazılacağı void** işaretçisi.
+// Dönüş: Başarı veya hata belirten c_ampersand_result.
 c_ampersand_result c_ampersand_allocate(size_t size, void **ptr);
 
-// Ayrılmış olan belleği serbest bırakır. (Otomatik bellek yönetimi olsa da gerekebilir)
-c_ampersand_result c_ampersand_free(void *ptr);
+// Daha önce c_ampersand_allocate ile ayrılmış olan belleği serbest bırakır.
+// Bellek Sahne64 API'si kullanılarak serbest bırakılır.
+// ptr: Serbest bırakılacak bellek bloğunun adresi.
+// size: Serbest bırakılacak bellek bloğunun boyutu (tahsis edilen boyutla aynı olmalı).
+// Dönüş: Başarı veya hata belirten c_ampersand_result.
+// NOT: Çöp toplama (GC) kullanıldığında bu fonksiyonun nasıl entegre edileceği
+// C& dilinin bellek modeline bağlıdır (örn. GC dışı manuel yönetim için kullanılabilir).
+c_ampersand_result c_ampersand_free(void *ptr, size_t size);
 
 // ... diğer bellek yönetimi ile ilgili fonksiyonlar eklenebilir (örneğin, yeniden boyutlandırma)
 
